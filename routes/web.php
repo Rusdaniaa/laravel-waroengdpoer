4ab4f581f11ed1bf809a48f523d067f3c611e3ff
andnryynt15 <andnryynt159@gmail.com>
2023-05-06 08:00:46 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('/login', 'Auth\LoginController@login');
+Route::get('/login', 'LoginController@showLoginForm')->name('login');
+Route::post('/login', 'LoginController@login');
 
 
 Route::get('/', function () {
