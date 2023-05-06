b03ddba5f5bd240cac6ca6f972ec0a45b5166a55
andnryynt15 <andnryynt159@gmail.com>
2023-05-06 07:58:33 +0700

    commit login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
