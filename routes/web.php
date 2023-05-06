1f5e73e455221ed96ead0df030b45f9b02a7f7b7
andnryynt15 <andnryynt159@gmail.com>
2023-05-06 08:14:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
     return view('views.login');
-});
+})->name('views.login');
 
 Route::get('/', function () {
     return view('welcome');
