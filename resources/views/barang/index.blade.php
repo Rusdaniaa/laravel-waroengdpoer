cc3eab6656df013b50a9f7df09563518fafaf22f
andnryynt15 <andnryynt159@gmail.com>
2023-05-10 14:22:16 +0700

    Update index.blade.php

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -11,12 +11,12 @@
                                 <div class="page-title-box">
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Projects</a></li>
-                                            <li class="breadcrumb-item active">Create Project</li>
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Waroeng D'poer</a></li>
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Kasir</a></li>
+                                            <li class="breadcrumb-item active">Cetak</li>
                                         </ol>
                                     </div>
-                                    <h4 class="page-title">Create Project</h4>
+                                    <h4 class="page-title">Kasir</h4>
                                 </div>
                             </div>
                         </div>     
