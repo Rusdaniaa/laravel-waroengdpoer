7c4ab6a6a5725f2e77cb66cb249fb8bd49912f4a
andnryynt15 <andnryynt159@gmail.com>
2023-06-05 22:21:54 +0700

    Update laporan.php

--- app/Models/laporan.php
+++ app/Models/laporan.php
@@ -5,17 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Laporan extends Model
+class laporan extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'id_transaksi', 'id_pelanggan', 'total_bayar', 'metode_pembayaran'
-    ];
-
-    // Metode akses untuk mendapatkan URL gambar
-    public function getGambarUrlAttribute()
-    {
-        return asset('images\products' . $this->nama_barang);
-        
-    }
 }
